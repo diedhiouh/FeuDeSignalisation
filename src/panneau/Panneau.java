9d2178757c6db557cd1f866e7ff459bcb1ceaa93
Habibou DIEDHIOU <habibodh@gmail.com>
2019-03-30 20:50:25 +0000

    Struture de base des classes d'Etat

--- src/panneau/Panneau.java
+++ src/panneau/Panneau.java
@@ -2,7 +2,27 @@ package panneau;
 
 public class Panneau {
 
+	//Etat du feu de signalisation
 	
+	public EtatFeu feu;
+	public int getNiveau() {
+		return niveau;
+	}
+	public void setNiveau(int niveau) {
+		this.niveau = niveau;
+	}
+	public int niveau;
+	
+	public Panneau(EtatFeu feu) {
+	    super();
+	    this.feu = new FeuVert();
+    }
+	public EtatFeu getFeu() {
+		return feu;
+	}
+	public void setFeu(EtatFeu feu) {
+		this.feu = feu;
+	}
 	public Panneau() {
 		// TODO Auto-generated constructor stub
 	}
